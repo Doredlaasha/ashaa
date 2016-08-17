07f8b82072207d47eca40e1d3371c569d522a880
Doredlaasha <doredlaasha@gmail.com>
2016-08-17 20:19:18 +0530

    palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,r,sum=0;
+printf("enter a number");
+canf("%d",&num);
+for(temp=num,num!=0,num=num/10)
+{
+r=num%10;
+sum=sum*10+r;
+}
+if temp==sum;
+printf("number is a palindrome");
+else
+printf("number is not a palindrome")
+return 0;
+}
